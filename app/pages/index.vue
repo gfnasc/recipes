ec3d612d4c95ef5b08df510fa10b90a6f76dcead
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-09-12 21:52:35 -0300

    feat: make dimple css ajustments

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,10 +10,10 @@ const { data, error } = await useFetch<RecipesResponse>(
   <main>
     <section class="bg-cream">
       <div
-        class="container flex flex-col lg:flex-row items-center py-20 gap-10"
+        class="container flex flex-col lg:flex-row items-center py-10 gap-10"
       ></div>
     </section>
-    <section class="py-20 container">
+    <section class="py-10 container">
       <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
       <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes!</p>
       <div
