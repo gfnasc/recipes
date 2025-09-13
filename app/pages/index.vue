f82637332f294ae807ef579616341ccc5b756c0e
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-09-12 21:35:19 -0300

    feat: remove hero section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,30 +11,7 @@ const { data, error } = await useFetch<RecipesResponse>(
     <section class="bg-cream">
       <div
         class="container flex flex-col lg:flex-row items-center py-20 gap-10"
-      >
-        <div class="flex-1 order-2 lg:order-1 text-center lg:text-left">
-          <h1 class="text-4xl lg:text-6xl font-extrabold mb-6 text-balance">
-            Master the Kitchen with Ease: Unleash Your Inner Chef Today!
-          </h1>
-          <p class="text-xl lg:text-2xl mb-8 text-balance">
-            Discover recipes helping you to find the easiest way to cook.
-          </p>
-          <button
-            class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-lg cursor-pointer"
-          >
-            Browse Recipes
-          </button>
-        </div>
-        <div class="flex-1 order-1 lg:order-2">
-          <NuxtImg
-            src="/nuxt-course-hero.png"
-            densities="x1"
-            format="webp"
-            sizes="xs:100vw sm:667px"
-            alt=""
-          />
-        </div>
-      </div>
+      ></div>
     </section>
     <section class="py-20 container">
       <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
