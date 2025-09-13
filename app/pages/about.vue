e932bcfd3958bbf457fa9f63e221f4e9fc100c93
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-09-12 21:16:22 -0300

    feat: update about text

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,13 +3,14 @@
 <template>
   <main>
     <section class="container py-20">
-      <h1 class="text-6xl font-extrabold mb-6 text-balance">
-        About Nuxtcipes:
-      </h1>
+      <h1 class="text-6xl font-extrabold mb-6 text-balance">About Recipes:</h1>
       <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
-        Nuxtcipes is a recipe application based on the dummyJSON API, created by
-        John Komarnicki for the
-        <span class="font-bold">Build Modern Apps with Nuxt</span> course.
+        Recipes is a recipe app (really?) based on the dummyJSON API, created by
+        Guilherme Nascimento during John Komarnicki's Build Modern Apps with
+        Nuxt crash course.
+      </p>
+      <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
+        It was created to review and refresh Nuxt concepts.
       </p>
     </section>
   </main>
