37885bcda147035db7fa3d8c95e10bfd92316f7d
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-09-16 18:33:54 -0300

    todo

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,8 @@
     </NuxtLayout>
   </div>
 </template>
+
+/* TO DO: * Adicionar um rodapé * Criar página de detalhes da receita * Filtros:
+Criar filtro por tags e ordenação por parâmetros da receita * Refinar design *
+Melhorar o readme adicionando tecnologias, funcionalidades e finalidade da
+criação do projeto */
